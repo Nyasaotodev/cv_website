7056ce76874290c0416bec7521a107bba74b73ac
Lohann <lohann.allostry@gmail.com>
2024-02-08 20:40:44 +0100

    first update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <header>
+          <div className={"bg-slate-900 "}>
+            <div className={"flex justify-center p-10 bg-slate-700 max-w-xl rounded-r-full "}>
+              <div className={"flex flex-col text-slate-100 space-y-3 my-5"}>
+                <h3 className={"text-5xl "}>Lohann Allostry</h3>
+                <p className={"text-2xl"}>Alternance BUT Informatique</p>
+              </div>
+              <img src={logo} className=" h-28 w-28 mt-3 ml-10" alt="logo" />
+
+            </div>
+
+          </div>
       </header>
     </div>
   );
